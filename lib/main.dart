0006a4cf0d8fb569b03012b9b7f092e0c4a60434
excelitai-tusher <tusher@excelitai.com>
2021-12-21 16:34:41 +0600

    commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:animation_3d/drawer_3d.dart';
+//import 'package:drawer3d/drawer_3d.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Drawer3D(),
+    );
+  }
+}
